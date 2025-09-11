9180d132483a44496df65a6b7d5ca8a7d99e8125
Patrick Ambrose (patppuccin) <patrickambrosericky@gmail.com>
2025-09-11 14:34:46 +0530

    feat(core): add terminal space helper
    - Added makeSpace function to ensure enough room for rendering prompts
    - Moves the cursor down to guarantee the full prompt is visible

--- asky_utils.go
+++ asky_utils.go
@@ -9,3 +9,15 @@ import (
 func getTermDimensions() (int, int, error) {
 	return term.GetSize(int(os.Stdout.Fd()))
 }
+
+func makeSpace(lines int) error {
+	width, height, _ := getTermDimensions()
+	if height < lines || width < 50 {
+		return ErrTerminalTooSmall
+	}
+	for range lines {
+		os.Stdout.WriteString("\n")
+	}
+	ansiCursorUp(lines)
+	return nil
+}
