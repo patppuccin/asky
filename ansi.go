ef4c8602cba0bf90cca08d64f43e7355aea3ea2c
Patrick Ambrose (patppuccin) <patrickambrosericky@gmail.com>
2026-03-21 20:25:52 +0530

    refactor: cleanup unused ansi helpers

--- ansi.go
+++ ansi.go
@@ -5,22 +5,13 @@ import (
 )
 
 const (
-	ansiHideCursor    = "\033[?25l"
-	ansiShowCursor    = "\033[?25h"
-	ansiSaveCursor    = "\0337"
-	ansiRestoreCursor = "\0338"
+	ansiHideCursor = "\033[?25l"
+	ansiShowCursor = "\033[?25h"
 
 	ansiReset       = "\033[0m\033[0 q"
 	ansiClearLine   = "\033[K"
 	ansiClearScreen = "\033[J"
 )
-
-// ansiCursorLeft moves the cursor n positions to the left.
-func ansiCursorLeft(n int) {
-	if n > 0 {
-		stdOutput.Write([]byte("\033[" + strconv.Itoa(n) + "D"))
-	}
-}
 
 // ansiCursorUp moves the cursor n positions up.
 func ansiCursorUp(n int) {
